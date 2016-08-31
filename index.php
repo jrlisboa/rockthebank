53ddfe692ffbedfe540baf4f5c75dcdfa3cb64ec
Junior Lisboa <juniorzk2910@gmail.com>
2016-08-31 18:59:57 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>RockTheBank</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+ <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <main style="">
+<div class="head hero">
+<div class="">
+
+
+    <div class="header clearfix menu">
+
+        <nav role="navigation navbar pull-right col-md-offset-4">
+          <!--<div class="navbar-header">
+              <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+
+          </div> -->
+          <div class="">
+
+
+          <ul class="nav nav-pills pull-right col-md-5">
+            <li role="presentation" class="ativo"><a href="index.html">HOME</a></li>
+            <li role="presentation"><a href="sobre.html">SOBRE</a></li>
+            <li role="presentation"><a href="#">CONTATO</a></li>
+            <li role="presentation" class="btnav"><a href="#">COMEÇAR AGORA</a></li>
+          </ul>
+
+            </div>
+            <img class="logo img-responsive pull-left col-md-offset-1" src="img/logo.png"/>
+        </nav>
+
+      </div>
+
+  </div>
+  <div class="col-md-7 col-md-offset-4 " style="height:100%">
+
+
+  <div class="anuncio-div" >
+    <span class="anuncio centred">O Poder das finanças em suas mãos.</span><br>
+    <span class="anuncio">Simples, Rápido e divertido.</span>
+    <div class="row-fluid col-md-offset-3" >
+      <div class="col-md-4 btnheader ">
+        <div class="padrao">Saber mais</div>
+      </div>
+      <div class="col-md-4 btnheader ">
+        <div class="azul ">Comece agora</div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  </div>
+</main>
+
+
+<section class="explicacao">
+<div class="container text-center">
+  <h1 class="explicacao-title">Hoje as finanças são assim:</h1>
+  <div class="row container col-md-offset-2 receita-investimento ">
+    <div class="col-md-2 receita" >
+      <img src="assets/home/icon01.png"/><br><br>
+      <span class="legenda">Receitas - Gastos</span>
+    </div>
+
+    <div class="col-md-2 investimento " style="margin-left:50px;">
+      <img src="assets/home/icon02.png"/><br><br>
+      <span class="legenda">Investimento</span>
+    </div>
+    <div class="col-md-2 investimento">
+      <img src="assets/home/icon03.png"/><br><br>
+      <span class="legenda">Créditos</span>
+    </div>
+    <div class="col-md-2 investimento">
+      <img src="assets/home/icon04.png"/><br><br>
+      <span class="legenda">Quitação de dividas</span>
+    </div>
+  </div>
+  <div class="row container col-md-offset-2">
+    <div class="col-md-2">
+
+      <h2 class="textos1"><strong>Fluxo</strong></h2>
+    </div>
+
+    <div class="col-md-6 estoque">
+      <h2 class="textos1"><strong>Estoque</strong></h2>
+    </div>
+
+  </div>
+</div>
+<div class="container text-center receita-investimento">
+  <h1 class="explicacao-title">No futuro, novas soluções tornarão o mundo<br> das finanças mais acessivel para as pessoas</h1>
+  <div class="row container receita-investimento imagens-receita">
+    <div class="col-md-2 "  >
+      <img src="assets/home/icon05.png"/><br><br>
+      <span class="legenda">Simuladores</span>
+    </div>
+
+    <div class="col-md-2  " style="margin-left:50px;">
+      <img src="assets/home/icon06.png"/><br><br>
+      <span class="legenda">Projeções</span>
+    </div>
+    <div class="col-md-2 ">
+      <img src="assets/home/icon07.png"/><br><br>
+      <span class="legenda">Planejamento</span>
+    </div>
+    <div class="col-md-2 ">
+      <img src="assets/home/icon08.png"/><br><br>
+      <span class="legenda">Banking game</span>
+    </div>
+  </div>
+
+</div>
+<br><br><br><br>
+</section>
+  <section class="solucao text-center">
+    <div style="height:20vh"></div>
+    <span id="textopara">O RockTheBank é uma solução completa para hoje<br>e tambem para o futuro. Buscamos a    simplicidade<br>para devolver o poder das finanças pra você</span>
+  </section>
+<section class="map text-center">
+  <span class="tit-map" ><strong>Vamos juntos mudar sua vida<br>e quem sabe do Brasil todo.</strong></span><br><br><br><br><br><br>
+  <span class="text-map" >O Brasil tem a maior taxa de juros do mundo.<br>61% das famílias brasileiras estão com dívidas.<br>R$ 800 bilhões de investimentos que<br>poderiam ter melhores retornos.<br><br><strong>Juntos vamos mudar essa situação</strong></span>        
+</section>
+
+  <div class="yellow-icons">
+    <section class="uma-solucao text-center completa container" style="padding-top: 100px; ">
+      <span class="tit-map " ><strong>UMA SOLUÇÃO COMPLETA</strong></span><br><br><br><br><br><br>
+      <div style="margin-left: 150px;">
+      <div class="col-md-6 icone"  >
+          <img class="imagem-icon"  src="assets/home/icon09.png"/><br><br>
+          <span class="legenda" style="font-size: 20px; color: #19274C"><strong>CONHECIMENTO</strong></span><br><br>
+          <span class="legenda">Reunimos todas as informações sobre<br>finanças pessoais em um só lugar, de<br>controle de gastos a investimentos.</span>
+        </div>
+
+        <div class="col-md-6  icone" >
+          <img class="imagem-icon" src="assets/home/icon10.png"/><br><br>
+          <span class="legenda" style="font-size: 20px; color: #19274C"><strong>SOLUÇÕES</strong></span><br><br>
+          <span class="legenda" align="left">Criamos ferramentas, simuladores e <br>aplicativos para facilitar a sua vida e<br>te dar o controle das suas finanças.</span>
+        </div>
+        <div class="col-md-6 icone">
+          <img class="imagem-icon" src="assets/home/icon11.png"/><br><br>
+          <span class="legenda" style="font-size: 20px; color: #19274C"><strong>CONEXÕES</strong></span><br><br>
+          <span class="legenda">Vamos te conectar a pessoas e lugares<br>estratégicos para te ajudar a atingir um<br>equilíbrio financeiro.</span>
+        </div>
+        <div class="col-md-6 icone">
+          <img  class="imagem-icon" src="assets/home/icon12.png"/><br><br>
+          <span class="legenda" style="font-size: 20px; color: #19274C"><strong>TEMPO</strong></span>  <br><br>
+          <span class="legenda">Seu tempo é valioso. Aqui você<br>escolhe quanto quer dedicar por dia<br>para investir no seu futuro.</span>  
+          </div>
+          </div>
+    </section>
+  </div>
+
+<section class="solucao2 text-center">
+    <div style="height:20vh"><span id="textopara" style="font-size: 22px;"><strong>Não somos um banco.</strong><br><br>Acreditamos que suas finanças devem estar nas<br>suas mãos. Criamos soluções para que você faça<br>isso de modo simples, rápido e divertido.<br><br></span>
+    <a href="#"><div class="col-md-4 btnheader " style="margin-left: 500px;">
+        <div class="padrao">Saiba mais sobre nós</div>
+      </div></a></div>
+  </section>
+
+<div>
+    <section class="uma-solucao text-center completa container" style="padding-top: 100px; ">
+      <br><br><br><br><br><br>
+      <div style="margin-left: 150px;">
+      <div class="col-md-5 icone"  >
+          <img style="margin-left: -70px; margin-top: -140px;" src="assets/home/imagem05.png"/><br><br>
+          
+        </div>
+
+        <div class="col-md-7 comece-div icone" >
+          <span class="comece" align="left">Comece já conhecendo suas finanças<br>para ter mais controle e economizar dinheiro.<br><br><br><strong>Baixe o app gratuito.<br><br><br></strong></span>
+          <img style="width: 200px;" src="assets/home/ios.png"/>
+          <img style="width: 200px; margin-left: 50px;" src="assets/home/android.png"/>
+        </div>
+    </section>
+  </div>  
+</section>
+
+<section class="solucao3 ">
+    <div class="col-md-4 contato">
+      <div class="">
+        <span class="contato-tit">CONTATO<br><br></span>
+        <div class="contato-form">
+          <form action="mail_send.php" method="post">
+          <label>Nome</label><br>
+          <input required type="text" name="nome"><br><br>
+          <label>Email</label><br>
+          <input required type="text" name="email"><br><br>
+          <label>Mensagem</label><br>
+          <textarea required name="mensagem"></textarea>
+          <button type="submit" name="BTEnvia" class="azul enviar">Enviar</button>
+        </form>
+        </div>
+      </div>
+    </div>
+
+
+
+    <footer class="container">
+    <img class="logo footer-logo" src="img/logo.png"/>
+      <span class="mini-espaco-footer">Rua Casa do Ator, 909 - 00000000 - São Paulo - SP</span>
+      <span class="espaco-footer">(11) 0000-0000</span>
+    </footer>
+  </section>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>RockTheBank</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+ <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <main style="">
+<div class="head-sobre hero">
+<div class="">
+
+
+    <div class="header clearfix menu">
+
+        <nav role="navigation navbar pull-right col-md-offset-4">
+          <!--<div class="navbar-header">
+              <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+
+          </div> -->
+          <div class="">
+
+
+          <ul class="nav nav-pills pull-right col-md-5">
+            <li role="presentation"><a href="index.html">HOME</a></li>
+            <li role="presentation" class="ativo"><a href="sobre.html">SOBRE</a></li>
+            <li role="presentation"><a href="#">CONTATO</a></li>
+            <li role="presentation" class="btnav"><a href="#">COMEÇAR AGORA</a></li>
+          </ul>
+
+            </div>
+            <img class="logo img-responsive pull-left col-md-offset-1" src="img/logo.png"/>
+        </nav>
+
+      </div>
+
+  </div>
+  <div class="col-md-7 col-md-offset-4 " style="height:100%">
+
+
+  <div class="anuncio-div-sobre" >
+    <span class="anuncio centred"> NÃO SOMOS UM BANCO.<br> CRIAMOS SOLUÇÕES PARA TE<br>AJUDAR A TER TRANQUILIDADE<br>FINANCEIRA. SIMPLES ASSIM.</span><br>
+    <div class="row-fluid col-md-offset-3" >
+      <div class="col-md-4 btnheader ">
+        <div class="padrao-sobre">Saber mais</div>
+      </div>
+      <div class="col-md-4 btnheader ">
+        <div class="azul-sobre ">Comece agora</div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  </div>
+</main>
+
+
+<section class="explicacao">
+<div class="container text-center">
+  <h1 class="acreditamos-title">EM QUE ACREDITAMOS</h1>
+  
+  <p align="left" class="paragrafo">1. Não somos um banco <br><br>
+    2. Procuramos simplicidade, clareza e rapidez. <br><br>
+    3. Transferimos o poder da decisão para você. <br><br>
+    4. Acreditamos que melhorar suas finanças, significa: Ter<br>
+    mais tempo para você, menos stress dormindo tranquilo e<br>
+    vencer o medo. Ou seja, ter uma vida muito mais feliz, saudável<br>
+    e com muito mais dinheiro.<br><br>
+    5. Queremos curar as doenças financeiras e não dar<br>
+    soluções de paliativas como baixar juros ou zerar<br>
+    tarifas. Não queremos pessoas dependentes de remédios<br>
+    financeiros pouco eficazes para o restante da sua vida.<br><br>
+    6. Sabemos que finanças é chato. Queremos mudar isso.<br><br>
+    7. Queremos causar um impacto na sociedade. Sabemos<br>
+    que somente conseguiremos se ajudarmos você.<br><br>
+    8. Vamos compartilhar as soluções que criamos com o<br>
+    maior número de pessoas.<br><br>
+    9. Estaremos em todos os lugares e, principalmente, na<br>
+    base (escolas e comunidades).<br><br>
+    10. Vamos ajudar todos a atingir um equilobrio financeiro<br>
+    e viver melhor.</p>
+</div>
+<br><br><br><br>
+</section>
+  <section class="solucao-sobre text-center">
+    <div class="container text-center">
+    <h1 class="acreditamos-title2">COMO GANHAMOS</h1>
+    
+    <p align="left" class="paragrafo2">A maior parte do acesso às ferramentas é <strong>gratuita.</strong> A<br>
+      solução completa tem um custo acessível de apenas<br>
+      R$ XX ṕr mês.<br><br>
+      Ademais queremos ajudar:<br><br>
+      - Quem tem mais problemas / Os menos afortunados:<br>
+      7,5% da nossa renda líquida (depois de todos os gastos e im-<br>
+      postos) serão destinados para quitar dívidas de pessoas<br>
+      com critérios bem definidos e claros.<br><br>
+      - Quem pode influenciar nosso futuro: 2,5% da nossa<br>
+      renda líquida será destinado para os melhoresprofes-<br>
+      sores e alunos de matemática. Para cada pacote de Bank-<br>
+      ing Game vendido para uma esocla privada será doado<br>
+      um para uma escola pública automaticamente (Critérios claros e<br>
+      bem definidos de seleção).</p>
+  </div>
+  </section>
+
+
+<div>
+    <section class="uma-solucao text-center completa container" style="padding-top: 100px; margin-top: 10px;">
+      <br><br><br><br><br><br>
+      <div style="margin-left: 150px;">
+      <div class="col-md-5 icone"  >
+          <img style="margin-left: -70px; margin-top: -140px;" src="assets/home/imagem05.png"/><br><br>
+          
+        </div>
+
+        <div class="col-md-7 comece-div icone" >
+          <span class="comece" align="left">Comece já conhecendo suas finanças<br>para ter mais controle e economizar dinheiro.<br><br><br><strong>Baixe o app gratuito.<br><br><br></strong></span>
+          <img style="width: 200px;" src="assets/home/ios.png"/>
+          <img style="width: 200px; margin-left: 50px;" src="assets/home/android.png"/>
+        </div>
+    </section>
+  </div>  
+</section>
+
+<section class="solucao3 ">
+    <div class="col-md-4 contato">
+      <div class="">
+        <span class="contato-tit">CONTATO<br><br></span>
+        <div class="contato-form">
+          <form>
+          <label>Nome</label><br>
+          <input type="text" name="nome"><br><br>
+          <label>Email</label><br>
+          <input type="text" name="email"><br><br>
+          <label>Mensagem</label><br>
+          <textarea></textarea>
+          <button class="azul enviar">Enviar</button>
+        </form>
+        </div>
+      </div>
+    </div>
+
+    <footer class="container">
+    <img class="logo footer-logo" src="img/logo.png"/>
+      <span class="mini-espaco-footer">Rua Casa do Ator, 909 - 00000000 - São Paulo - SP</span>
+      <span class="espaco-footer">(11) 0000-0000</span>
+    </footer>
+  </section>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
